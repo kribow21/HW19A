809e01f3737f861cfe2725177f6b29bb47d315d3
kribow21 <kristininnotech@gmail.com>
2021-09-20 12:50:19 -0600

    imported modules, now to figure out how to structure this

---
+++ app.py
@@ -0,0 +1,10 @@
+import addition
+import subtraction
+import multiplication
+import division
+
+option_one = print(addition.add_calculation())
+option_two = print(subtraction.subtract_calculation())
+option_three = print(multiplication.multiply_calculation())
+option_four = print(division.divide_calculation())
+
