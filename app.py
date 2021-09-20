e231ef9260b99e4ef353622625a45f82b74105d0
kribow21 <kristininnotech@gmail.com>
2021-09-20 14:16:56 -0600

    completed calculator

--- app.py
+++ app.py
@@ -3,8 +3,25 @@ import subtraction
 import multiplication
 import division
 
-option_one = print(addition.add_calculation())
-option_two = print(subtraction.subtract_calculation())
-option_three = print(multiplication.multiply_calculation())
-option_four = print(division.divide_calculation())
+print("Please select an option for the calculation")
+print("1 : Addition")
+print("2 : Subtraction")
+print("3 : Multiplication")
+print("4 : Division")
+user_calculation_choice = int(input())
+print("Please enter your first number")
+user_num_one = int(input())
+print("Please enter your second number")
+user_num_two = int(input())
+if (user_calculation_choice == 1):
+    result = addition.add_calculation(user_num_one, user_num_two)
+elif(user_calculation_choice == 2):
+    result = subtraction.subtract_calculation(user_num_one, user_num_two)
+elif(user_calculation_choice == 3):
+    result = multiplication.multiply_calculation(user_num_one, user_num_two)
+elif(user_calculation_choice == 4):
+    result = division.divide_calculation(user_num_one, user_num_two)
+print("Your result is : " +str(result))
+
+
 
