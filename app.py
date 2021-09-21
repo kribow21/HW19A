fe3babbcd887680faf4f5f7e6c55dac249d36dcd
kribow21 <kristininnotech@gmail.com>
2021-09-20 20:10:12 -0600

    realized i could add text in the input and didnt need the extra print statments

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ import subtraction
 import multiplication
 import division
 
-print("Please select an option for the calculation")
 print("1 : Addition")
 print("2 : Subtraction")
 print("3 : Multiplication")
 print("4 : Division")
-user_calculation_choice = int(input())
-print("Please enter your first number")
-user_num_one = int(input())
-print("Please enter your second number")
-user_num_two = int(input())
+user_calculation_choice = int(input("Please select an option for the calculation : "))
+user_num_one = int(input("Please enter your first number : "))
+user_num_two = int(input("Please enter your second number : "))
 if (user_calculation_choice == 1):
     result = addition.add_calculation(user_num_one, user_num_two)
 elif(user_calculation_choice == 2):
